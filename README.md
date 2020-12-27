89a98eb2f25f3c0972105a154592442888f04772
dikshitakambri <dikshitakambri18@gmail.com>
2020-12-27 22:41:04 +0530

    drum kit

---
+++ README.md
@@ -0,0 +1,14 @@
+<h1>Drum Kit 🥁</h1>
+
+<p>In this Drum kit, there are 7 buttons and whenever you click on any button or press mentioned key 
+   then it will produces sound.🎼</p>
+
+<h4>Languages Used</h4>
+<ul>
+    <li>HTML</li>
+    <li>CSS</li>
+    <li>Javascript</li>
+</ul>
+
+<img src="Images/snap.png" width="50%">
+
---
+++ index.js
@@ -0,0 +1,55 @@
+var numberOfDrumButton = document.querySelectorAll(".drum").length;
+
+for( var i=0; i < numberOfDrumButton; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function() {
+
+    var buttoninnerHTML = this.innerHTML;
+
+    makesound(buttoninnerHTML);
+
+});
+}
+document.addEventListener("keypress",function(event) {
+    makesound(event.key);
+});
+
+function makesound(key){
+    switch (key){
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        
+        case "a":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        
+        case "s":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+        case "d":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+
+        case "j":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        
+        case "k":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        
+        case "l":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+        default:
+    }
+}
